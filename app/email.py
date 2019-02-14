77052fdc6036a1a93589d8f7e9c3028b61e6618a
Choiyounseo <dbstj7212@naver.com>
2019-02-14 15:12:23 +0900

    part13

--- app/email.py
+++ app/email.py
@@ -1,8 +1,8 @@
 from flask_mail import Message
-from app import mail
 from flask import render_template
-from app import app
+from app import app, mail
 from threading import Thread
+from flask_babel import _
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -16,7 +16,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password',
+    send_email(_('[Younsblog] Reset Your Password'),
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
